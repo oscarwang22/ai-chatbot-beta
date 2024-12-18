092244c00e77a0d67ffdc74191985e721e1fc8e1
Oscar Wang <141192440+oscarwang22@users.noreply.github.com>
2024-12-18 21:05:24 +1300

    Update index.ts

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,4 +1,4 @@
-import { google } from '@ai-sdk/google-generative-ai';
+import { google } from '@ai-sdk/google';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
